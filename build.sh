2a0d860a15d894d96fc53df83c162272b5a70ab0
Einsiedler90 <einsiedler90@protonmail.com>
2022-05-12 20:18:14 +0200

    set path to dependencies in bash

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ INSTALLER_VERSION="200"
 MANUFACTURE="ENCRYPTED SUPPORT LP"
 DESCRIPTION="Whonix Starter"
 
+FILE_LICENSE="../deps/license.txt"
+FILE_WHONIX_OVA="../deps/Whonix.ova"
+FILE_WHONIX_EXE="../deps/Whonix.exe"
+FILE_VBOX_INST_EXE="../deps/vbox.exe"
+
 if ! [ -x "$(command -v wixl)" ]; then
   echo 'Error: wixl is not installed.' >&2
   exit 1
@@ -15,6 +20,10 @@ wixl -v --arch x64 \
   -D whonixInstallerVersion="$INSTALLER_VERSION" \
   -D whonixManufacturer="$MANUFACTURE" \
   -D whonixDescription="$DESCRIPTION" \
+  -D whonixFileLicense="$FILE_LICENSE" \
+  -D whonixFileOva="$FILE_WHONIX_OVA" \
+  -D whonixFileMainExe="$FILE_WHONIX_EXE" \
+  -D whonixFileVboxExe="$FILE_VBOX_INST_EXE" \
   Whonix.wxs
   
 exit 0
