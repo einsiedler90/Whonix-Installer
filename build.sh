6ca03b5e363daf7f502da2e80c6a6ce7ec9b7d13
Einsiedler90 <einsiedler90@protonmail.com>
2023-04-26 18:06:41 +0200

    whonix starter setup changes

--- build.sh
+++ build.sh
@@ -3,15 +3,18 @@
 set -x
 set -e
 
-VERSION="16.0.4.2-empty"
-INSTALLER_VERSION="200"
+VERSION="16.0.9.8"
+INSTALLER_VERSION="210" # increase for every different VERSION
 MANUFACTURE="ENCRYPTED SUPPORT LP"
 DESCRIPTION="Whonix Starter"
 
 FILE_LICENSE="../deps/license.txt"
-FILE_WHONIX_OVA="../deps/Whonix.ova"
+#FILE_WHONIX_OVA="../deps/Whonix.ova"
+FILE_WHONIX_OVA="../deps/Whonix-XFCE-16.0.9.8.ova"
 FILE_WHONIX_EXE="../deps/Whonix.exe"
 FILE_VBOX_INST_EXE="../deps/vbox.exe"
+
+# 1) build msi package for whonix starter
 
 if ! [ -x "$(command -v wixl)" ]; then
   echo "$0: ERROR wixl is not installed." >&2
@@ -28,8 +31,26 @@ wixl \
   --define whonixManufacturer="$MANUFACTURE" \
   --define whonixDescription="$DESCRIPTION" \
   --define whonixFileLicense="$FILE_LICENSE" \
-  --define whonixFileOva="$FILE_WHONIX_OVA" \
   --define whonixFileMainExe="$FILE_WHONIX_EXE" \
-  --define whonixFileVboxExe="$FILE_VBOX_INST_EXE" \
-  --output ./Whonix.msi \
-  Whonix.wxs
+  --output WhonixStarterSetup.msi \
+  WhonixStarterSetup.wxs
+
+# 2) set current whonix OVA size in INI file for main setup executable
+
+FILE_WHONIX_OVA_SIZE=$(stat -c%s "$FILE_WHONIX_OVA")
+printf "[general]\nsize=$FILE_WHONIX_OVA_SIZE" > WhonixOvaInfo.ini
+
+# 3) update resource files
+
+cp $FILE_LICENSE LICENSE
+cp $FILE_VBOX_INST_EXE VBoxSetup.exe
+
+# 3) build executable for whonix setup
+
+lazbuild -B WhonixSetup.lpr --cpu=x86_64 --os=win64 --compiler=/usr/bin/ppcrossx64
+
+# 4) append whonix OVA to setup executable
+
+cat WhonixSetup.exe $FILE_WHONIX_OVA > WhonixSetup-XFCE-$VERSION.exe
+
+exit 0
