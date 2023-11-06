7041edffc70d40355a2f4fa296b3bb102ff78b50
Einsiedler90 <einsiedler90@protonmail.com>
2023-11-06 01:57:52 +0100

    new src dir for pascal code (tmp file issue)

--- build-windows-einsiedler.sh
+++ build-windows-einsiedler.sh
@@ -25,7 +25,7 @@ export FILE_LICENSE="../deps/license.txt"
 export FILE_WHONIX_OVA="../deps/Whonix-$VERSION_FULL.ova"
 export FILE_WHONIX_STARTER_MSI="../deps/WhonixStarterInstaller.msi"
 export FILE_VBOX_INST_EXE="../deps/vbox.exe"
-export FILE_INSTALLER_BINARY_FINAL="WhonixInstaller-$VERSION_FULL.exe"
+export FILE_INSTALLER_BINARY_FINAL="build/WhonixInstaller-$VERSION_FULL.exe"
 
 ./build.sh
 
