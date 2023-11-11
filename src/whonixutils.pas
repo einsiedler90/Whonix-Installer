f99b25c325954bccec7b9bf30a93dc050bc32233
Patrick Schleizer <adrelanos@whonix.org>
2023-11-11 14:50:57 -0500

    copyright

--- src/whonixutils.pas
+++ src/whonixutils.pas
@@ -3,20 +3,7 @@
  *
  * Copyright: 2012 - 2023 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
  * Author: einsiedler90@protonmail.com
- * License: GPL-3+-with-additional-terms-1
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * .
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * .
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *)
+ * License: See the file COPYING for copying conditions.
 
 unit WhonixUtils;
 
